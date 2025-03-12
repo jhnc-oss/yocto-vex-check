e5d02297c2942abd68717d71fc2788e7a48d04fc
h5b <h5b@users.noreply.github.com>
2025-03-12 15:43:28 +0100

    Revert "Add loop parameter to all update status impl."
    This reverts commit 0acf90b3d32e59a571005162f0ccf1df1d4292b6.

--- databases.py
+++ databases.py
@@ -370,7 +370,7 @@ class Database:
         pass
 
     @abstractmethod
-    def update_status(self, d, product, pv, pn, vendor, cve_data, cves_status, loop):
+    def update_status(self, d, product, pv, pn, vendor, cve_data, cves_status):
         pass
 
 
@@ -386,7 +386,7 @@ class NVDDatabase(Database):
         else:
             return None
 
-    def update_status(self, d, product, pv, pn, vendor, cve_data, cves_status, loop):
+    def update_status(self, d, product, pv, pn, vendor, cve_data, cves_status):
         # Find all relevant CVE IDs.
         has_cves_in_product = False
         real_pv = d.getVar("PV")
